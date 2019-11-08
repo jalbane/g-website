b39c8c5b9dc341ebe70efa6637a99c83c474f698
Jeff Albanese <jalbane9@gmail.com>
2019-11-07 20:53:51 -0500

    display changes

---
+++ src/Rankingslist.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './rankingslist.css'
+
+class Rankingslist extends React.Component{
+	render(){
+		
+		return (
+			<div>
+				<span className ='rankings-cell'>  </span>
+	    		<span className ='rankings-cell'> {this.props.raidSummary.summary} </span>
+	    		<span className ='rankings-cell'> {this.props.raidSummary.realm} </span>
+	    		<span className ='rankings-cell'> {this.props.raidSummary.world} </span>
+			</div>
+		);
+	}
+}
+
+export default Rankingslist
