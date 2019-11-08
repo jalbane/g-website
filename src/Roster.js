5e53e3f0a40d3887b7483829b0b6f35e8a7edabc
Jeff Albanese <jalbane9@gmail.com>
2019-11-07 21:01:27 -0500

    fixed link address

--- src/Roster.js
+++ src/Roster.js
@@ -16,7 +16,7 @@ class Roster extends Component{
 
 		return (
 				<div id = "row">
-					<span className = 'cell' ><a id = "redirect" href = {`https://raider.io/character/us/lightbringer/${this.props.itemList.raider[index].character.name}`} target = "_blank" rel="noopener noreferrer" ><img src = {`https://render-us.worldofwarcraft.com/character/${this.props.itemList.raider[index].character.thumbnail}`} alt = "char_thumbnail"/></a></span>
+					<span className = 'cell' ><a id = "redirect" href = {`https://raider.io/characters/us/lightbringer/${this.props.itemList.raider[index].character.name}`} target = "_blank" rel="noopener noreferrer" ><img src = {`https://render-us.worldofwarcraft.com/character/${this.props.itemList.raider[index].character.thumbnail}`} alt = "char_thumbnail"/></a></span>
 					<span className = 'cell' >{this.props.itemList.raider[index].character.name} </span>
 					<span className = 'cell' >{this.props.itemList.raider[index].character.spec.name} </span>
 					<span className = 'cell' >{spec} </span>
