1015a78ebef64bf7067c36d88e32e90cf0c013b1
Jeff Albanese <jalbane9@gmail.com>
2019-11-12 22:11:31 -0500

    Navabar changes

--- src/Home.js
+++ src/Home.js
@@ -20,19 +20,17 @@ class Home extends Component{
 	return(
 		<HashRouter history="browserHistory">	     
 
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="">React-Bootstrap</Navbar.Brand>
+        <Navbar bg="light" expand="lg" style = {{'min-height': '100px', 'font-size': '24px'}}>
+          <Navbar.Brand href="">NFA</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link ><Link to="" style = {{'color': 'black', 'textDecoration': 'none'}}>Rankings</Link></Nav.Link>
               <Nav.Link><Link to="editedApp" style = {{'color': 'black' , 'textDecoration': 'none'}}className = "disabled-link">Roster</Link></Nav.Link>
               <NavDropdown title="Additional Links" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="https://raider.io/guilds/us/lightbringer/NFA" target = "_blank">Raider.io</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.wowprogress.com/guild/us/lightbringer/NFA" target = "_blank">Wow progress</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.warcraftlogs.com/guild/id/357706" target = "_blank">Warcraft logs</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
